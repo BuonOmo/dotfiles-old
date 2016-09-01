070d056609376273638b9c08149db03c95d7b46b
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-08-31 23:10:03 -0400

    Clean comments

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ echo "You first need to install:
 - tmux
 - vim (vim-gnome edition is the best for copy pasting)"
 # sudo apt-get install vim-gnome tmux zsh xclip
-#read "Continue installation (Y/n)?" yn
-#if test $yn = "n" || test $yn = "N";then
-#	exit 1
-#fi
 echo Copying files
 ln -i zshrc ~/.zshrc
 ln -i vimrc ~/.vimrc
@@ -17,4 +13,4 @@ ln -i .tmux.conf ~/.tmux.conf
 ln -i functions/* ~/.oh-my-zsh/functions/
 find vim/* -exec ln -i {} ~/.{} \;
 echo Restart your terminal to finish installation
-
+#TODO : install vundles
